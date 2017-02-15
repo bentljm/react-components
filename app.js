3eab3283a89f906298815c124c7c0a47b2721e8c
Jeff Bentler <bentljm@gmail.com>
2017-02-14 19:37:07 -0800

    Finished basic requirements for sprint

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
-// TODO
+class AllGroceryListItems extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    };
+  }
+
+ mouseOver() {
+    this.setState({
+      hover: true
+    });
+  }
+
+ mouseOff() {
+    this.setState({
+      hover: false
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseOver={this.mouseOver.bind(this)} onMouseOff={this.mouseOff.bind(this)}>
+        {this.props.cookies}
+        </li>
+    );
+  }
+}
+
+var GroceryListItem = (props) => (
+
+  <ul>
+    {props.cookies.map(grocerylistitem =>
+      <AllGroceryListItems grocerylistitem={grocerylistitem} />
+    )}
+  </ul>
+
+);
+
+var GroceryList = () => (
+    <ul>
+      <h4>Grocery List Item</h4>
+      <GroceryListItem cookies={['Cucumber', 'Kale', 'Carrots', 'Celelery']}/>
+    </ul>
+
+)
+
+
+
+ReactDOM.render(
+    <GroceryList />,
+    document.getElementById('app')
+  );
+
